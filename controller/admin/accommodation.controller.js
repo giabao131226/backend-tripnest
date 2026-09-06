f8f4099b4ab515ed7e1060f32ff8caa9e8c4c17e
giabao131226 <dobao13122006@gmail.com>
2026-09-06 23:52:47 +0700

    Done: controller admin.acommodation.index

--- controller/admin/accommodation.controller.js
+++ controller/admin/accommodation.controller.js
@@ -55,12 +55,12 @@ module.exports.all = async (req, res) => {
     try {
         const find = {};
         const search = req.query.search;
-        if(search){
+        if (search) {
             find["$or"] = [];
             find["$or"].push({
-                name: {$regex: search,$options: "i"}
+                name: { $regex: search, $options: "i" }
             });
-            const [provinces,wards] = await Promise.all([
+            const [provinces, wards] = await Promise.all([
                 Province.find({
                     "name": {
                         $regex: search,
@@ -74,46 +74,62 @@ module.exports.all = async (req, res) => {
                     }
                 }),
             ])
-            if(provinces.length > 0){
+            if (provinces.length > 0) {
                 const idProvinces = provinces.map((item) => item._id);
                 find["$or"].push({
-                    "province_id": {$in: idProvinces}
+                    "province_id": { $in: idProvinces }
                 });
             }
-            if(wards.length > 0){
+            if (wards.length > 0) {
                 const idWards = wards.map((item) => item._id);
                 find["$or"].push({
-                    "ward_id": {$in: idWards}
+                    "ward_id": { $in: idWards }
                 });
             }
         }
 
         const category = req.query.category;
-        if(category && category != "all"){
+        if (category && category != "all") {
             find["category_id"] = category;
         }
+
         // Pagination
         let objectPagination = {
             currentPage: 1,
-            limitItems: 4
+            limitItems: 10
         }
         pagination(req.query, objectPagination)
         const countProduct = await BDS.countDocuments(find);
         const totalPage = Math.ceil(countProduct / objectPagination.limitItems)
         objectPagination.totalPage = totalPage
         //End Pagination
-        const accommodations = await BDS.find(find)
-            .limit(objectPagination.limitItems)
-            .skip((objectPagination.currentPage - 1) * objectPagination.limitItems)
-            .select("name ownerId category_id status")
-            .populate("ownerId", "username")
-            .populate("category_id");
-        
-        return res.json({ 
-            "success": true, 
+
+        const [accommodations, total,totalActive, totalInactive, totalPending, totalDenided] = await Promise.all([
+            await BDS.find(find)
+                .limit(objectPagination.limitItems)
+                .skip((objectPagination.currentPage - 1) * objectPagination.limitItems)
+                .select("name ownerId category_id status")
+                .populate("ownerId", "username")
+                .populate("category_id"),
+            await BDS.countDocuments(),
+            await BDS.countDocuments({"status": "active"}),
+            await BDS.countDocuments({"status": "in-active"}),
+            await BDS.countDocuments({"status": "pending"}),
+            await BDS.countDocuments({"status": "denided"})
+        ]);
+
+        return res.json({
+            "success": true,
             "accommodations": accommodations,
             "currentPage": objectPagination.currentPage,
             "totalPage": objectPagination.totalPage,
+            "overview": {
+                "totalActive": totalActive,
+                "totalInActive": totalInactive,
+                "totalPending": totalPending,
+                "totalDenided": totalDenided,
+                "total": total
+            }
         });
     } catch (ex) {
         console.log("Lỗi xảy ra ở controller admin.accommodation.all: " + ex);
