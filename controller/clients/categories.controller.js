24a1809b7729604b479eef2b28f5857bc17c49e5
giabao131226 <dobao13122006@gmail.com>
2026-09-17 23:32:33 +0700

    Done: Controller categories.controller.all

--- controller/clients/categories.controller.js
+++ controller/clients/categories.controller.js
@@ -20,10 +20,16 @@ module.exports.index = async (req, res) => {
 // [GET] "/categories/all"
 module.exports.all = async (req, res) => {
     try {
-        const {status,search} = req.query;
+        const { status, search } = req.query;
         const find = {};
-        if(status === 'active' || status === 'inactive') find.status = status;
-        console.log(find);
+        if (status === 'active' || status === 'inactive') find.status = status;
+        if (search != '') {
+            find.title = {
+                $regex: search,
+                "$options": "i"
+            };
+        }
+
         // Pagination
         let objectPagination = {
             currentPage: 1,
@@ -35,7 +41,16 @@ module.exports.all = async (req, res) => {
         objectPagination.totalPage = totalPage
         //End Pagination
 
-        const categories = await Category.find(find).lean();
+        const [categories, total,totalActive,totalInactive] = await Promise.all([
+            Category.find(find)
+                .limit(objectPagination.limitItems)
+                .skip((objectPagination.currentPage - 1) * objectPagination.limitItems)
+                .lean(),
+            Category.countDocuments(find),
+            Category.countDocuments({ "deleted": false, "status" : "active" }),
+            Category.countDocuments({ "deleted": false, "status": "inactive" }),
+        ])
+
         const data = await Promise.all(categories.map(async (category) => {
             const quantityAccLinkTo = await BDS.countDocuments({
                 "category_id": category._id
@@ -45,6 +60,11 @@ module.exports.all = async (req, res) => {
         return res.status(200).json({
             "success": true,
             "categories": data,
+            "overview": {
+                "totalActive": totalActive,
+                "totalInActive": totalInactive,
+                "total": total
+            },
             "currentPage": objectPagination.currentPage,
             "totalPage": objectPagination.totalPage
         });
